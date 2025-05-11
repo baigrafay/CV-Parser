1b8b9ce21f6530ce7e70e37e3af26dfe4fcf4b12
espark-maha <135619925+Maha927@users.noreply.github.com>
2025-05-11 14:31:11 +0500

    backend modified

--- ResumeCategorizer/settings.py
+++ ResumeCategorizer/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ResumeCategorizerApp',
-    "corsheader"
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -124,9 +124,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
